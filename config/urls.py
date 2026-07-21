a6748572bbf9967e3e1c546b4998832003f0653a
akshaii7 <akshaistackup@gmail.com>
2026-07-21 13:02:32 +0530

    Redirect root URL to Vercel frontend

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+def redirect_to_frontend(request):
+    return redirect('https://aethercart-frontend.vercel.app')
+
 urlpatterns = [
+    path('', redirect_to_frontend),
     path("admin/", admin.site.urls),
 
     path("api/accounts/", include("apps.accounts.urls")),
